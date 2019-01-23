56c198190e7934498b79a8f1bd7cea82a3321597
masayasviel <i.msya.tiger@gmail.com>
2019-01-23 11:49:18 +0900

    howとoutputの追加

---
+++ index.js
@@ -0,0 +1,13 @@
+function getId(ele){
+    let idValue = ele.id;
+    window.alert(data[idValue]);
+}
+
+var data = {
+    shootingGame : "phina.jsでつくったシューティングゲーム",
+    springRollRole : "春巻きが飛びます",
+    summerOrange : "ラブライブ！サンシャイン‼からアイディアをもらった",
+    typeGame : "PCで遊んでね",
+    NervousBreakdown : "同じ色を揃えよう",
+    chickenGame : "黒ひげのいない樽をあけよう"
+};
