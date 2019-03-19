82769faa51533439d156af884546d855c61f2acc
masayasviel <i.msya.tiger@gmail.com>
2019-03-19 11:50:57 +0900

    dataファイルにわけた

---
+++ data.js
@@ -0,0 +1,9 @@
+const data = {
+    shootingGame : "シューティングゲーム",
+    springRollRole : "春巻きが飛びます",
+    summerOrange : "木に当たらないようにミカンを集めよう",
+    typeGame : "タイピングゲーム\nPCで遊んでね",
+    NervousBreakdown : "同じ色を揃えよう",
+    chickenGame : "黒ひげのいない樽をあけよう",
+    pie_chart : "最大8つまで、スペースを入れずにカンマ区切りで半角数字を入力すると円グラフが生成されます"
+};
--- index.js
+++ index.js
@@ -1,14 +1,3 @@
 function getId(ele){
-    let idValue = ele.id;
-    window.alert(data[idValue]);
+    window.alert(data[ele.id]);
 }
-
-var data = {
-    shootingGame : "phina.jsでつくったシューティングゲーム",
-    springRollRole : "春巻きが飛びます",
-    summerOrange : "ラブライブ！サンシャイン‼からアイディアをもらった",
-    typeGame : "PCで遊んでね",
-    NervousBreakdown : "同じ色を揃えよう",
-    chickenGame : "黒ひげのいない樽をあけよう",
-    pie_chart : "最大8つまで、スペースを入れずにカンマ区切りで半角数字を入力すると円グラフが生成されます"
-};
