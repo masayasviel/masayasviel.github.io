b3db4150b16b6d9a71e72bc815763c178e70c619
masayasviel <i.msya.tiger@gmail.com>
2020-06-11 22:07:33 +0900

    add python3実行

--- js/data.js
+++ js/data.js
@@ -5,11 +5,10 @@ const panelData = [
     ["夏ミカン", "summerOrange", "./image/orange.png", "Aqours", "https://masayasviel.github.io/summerOrange/", "https://github.com/masayasviel/summerOrange"],
     ["神経衰弱", "nervousBreakdown", "./image/tyonoryoku.png", "神経衰弱", "https://masayasviel.github.io/NervousBreakdown/", "https://github.com/masayasviel/NervousBreakdown"],
     ["blackBeard", "chickenGame", "./image/taru.png", "黒ひげ危機一髪", "https://masayasviel.github.io/blackBeard-phina_js/", "https://github.com/masayasviel/blackBeard-phina_js"],
-    ["pieChart", "pieChart", "./image/pie-graph.png", "generatePieChart", "https://masayasviel.github.io/generate-pie-chart/", "https://github.com/masayasviel/generate-pie-chart"],
     ["crabEscape", "crabEscape", "./image/kani.png", "crabEscape", "https://masayasviel.github.io/crab_escape/", "https://github.com/masayasviel/crab_escape"],
     ["指グルグル", "spritesheetRikka", "./image/小鳥遊六花ちゃん.gif", "中二恋op指グルグルGIF", "https://masayasviel.github.io/spritesheetRikka/", "https://github.com/masayasviel/spritesheetRikka"],
     ["momiji", "momiji", "./image/momiji.png", "紅葉", "https://masayasviel.github.io/momiji/", "https://github.com/masayasviel/momiji"],
-    ["15puzzle", "_15puzzle", "https://3.bp.blogspot.com/-BDdu8TVY-pY/VHPgCghVdNI/AAAAAAAApOA/UtnEohNhOag/s400/jigsaw_puzzle.png", "15パズル", "https://masayasviel.github.io/_15puzzle/", "https://github.com/masayasviel/_15puzzle"]
+    ["Python3実行", "python3", "./image/python.png", "Python3", "https://masayasviel.github.io/online-compile-python/", "https://github.com/masayasviel/online-compile-python"]
 ];
 
 const howData = {
@@ -17,9 +16,8 @@ const howData = {
     summerOrange : "木に当たらないようにミカンを集めよう",
     nervousBreakdown : "同じ色を揃えよう",
     chickenGame : "黒ひげのいない樽をあけよう",
-    pieChart : "最大8つまで、スペースを入れずにカンマ区切りで半角数字を入力すると円グラフが生成されます",
     crabEscape : "逃げる蟹を捕まえよう",
     spritesheetRikka : "小鳥遊六花ちゃんグルグルgifをスプライトシートにしてphina.jsで動かしました\nロードが長ければ再読み込みしてください",
     momiji : "入力されたものの中からランダムに選択します",
-    _15puzzle : "15パズル"
+    python3 : "Python3のコードが実行できる"
 };
